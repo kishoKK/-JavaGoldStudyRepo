4e1b4b98f451cb659ea23bcc5ea97de381470991
kishoKK <kishogarieng@gmail.com>
2025-05-21 21:21:13 +0900

    コード修正(テスト)

--- src/test/hello.java
+++ src/test/hello.java
@@ -4,7 +4,8 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello World");
+		System.out.println("Hello Java!!");
+		System.out.println("Hello World!!");
 	}
 
 }
