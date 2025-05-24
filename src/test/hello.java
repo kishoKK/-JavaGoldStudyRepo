9b4da9423597224f2e34077708e10e59b0c9b358
kishoKK <kishogarieng@gmail.com>
2025-05-24 22:44:02 +0900

    for文追加

--- src/test/hello.java
+++ src/test/hello.java
@@ -3,9 +3,13 @@ package test;
 public class hello {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello Java!!");
-		System.out.println("Hello World!!");
+		// TODO 自動生成されたメソッド・スタブ[
+		
+		for(int i=1; i<=5; i++) {
+			
+			System.out.println(i+"回目："+"Hello!!");
+		}
+		
 	}
 
 }
