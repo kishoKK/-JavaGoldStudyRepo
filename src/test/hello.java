6b4955b6a88f72841b77091f9d94e636e287140b
kishoKK <kishogarieng@gmail.com>
2025-05-24 23:08:25 +0900

    "テスト"コメント追加

--- src/test/hello.java
+++ src/test/hello.java
@@ -4,7 +4,7 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ[
-		
+		System.out.println("テスト");
 		for(int i=1; i<=5; i++) {
 			
 			System.out.println(i+"回目："+"Hello!!");
